c01106e1db75a9d2563d3e5fd21f398947486cce
Javier Martinez <javiermarsal11@gmail.com>
2025-02-25 11:10:15 +0100

    Initial commit

---
+++ src/main/java/com/example/grandao_javiermartinez_giftomoigui/GranDaoJavierMartinezGiftOmoiguiApplication.java
@@ -0,0 +1,13 @@
+package com.example.grandao_javiermartinez_giftomoigui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranDaoJavierMartinezGiftOmoiguiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GranDaoJavierMartinezGiftOmoiguiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grandao_javiermartinez_giftomoigui/GranDaoJavierMartinezGiftOmoiguiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grandao_javiermartinez_giftomoigui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranDaoJavierMartinezGiftOmoiguiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
